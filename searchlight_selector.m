28b87b102911b0eed440bae046c1b9027dbee056
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-02-16 15:00:12 -0500

    Create searchlight_selector.m

---
+++ searchlight_selector.m
@@ -0,0 +1,38 @@
+function searchlight_selector(whichsearchlight, whichsubs, d)
+
+switch whichsearchlight
+    
+    % Subject-level searchlight
+    case 'subject'
+        for iSub = whichsubs(1):whichsubs(end)
+            d.iSub = iSub;
+            d.subName = d.subs(iSub).name;
+            searchlight(d)
+        end
+        
+    % Random-effects analysis of individual searchlights
+    case 'rfx'  
+        searchlightRFX(d)
+    
+    % Subject-level searchlights followed by a random-effects analysis
+    case 'subject_rfx'   
+        selectSearchlight('subject', whichsubs, d)
+        selectSearchlight('rfx', whichsubs, d)
+    
+    % Region-of-interest analysis
+    case 'roi'     
+        mask = spm_get_mat(d.maskFile);
+        nROI = max(unique(mask));
+        results = nan(d.nSubs, nROI);
+        for iSub = whichsubs(1):whichsubs(end)            
+            d.iSub = iSub;
+            d.subName = d.subs(iSub).name;
+            results = searchlightROI(d, results);
+        end
+        save([d.resultsDir '\ROI_' d.analysis], 'results', 'd')
+    
+    % Don't know what to do
+    otherwise
+        error('Input subject, rfx, subject_rfx, or roi.')
+        
+end
