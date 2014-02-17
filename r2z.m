d19ae298a0d381d8f8054376403cb136087d4a90
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-02-16 19:26:05 -0500

    Update r2z.m

--- r2z.m
+++ r2z.m
@@ -1,6 +1,6 @@
 function z = r2z(r)
 
-% Z = R2Z(R) takes as input a matrix of correlation coefficients R and
+% Z = R2Z(R) takes as input a single value or a correlation R and
 % applies a Fisher transformation. The resulting z-values are ouput in
 % matrix Z.
 %
@@ -9,28 +9,28 @@ function z = r2z(r)
 
 matrixDims = length(size(r));
 
-if matrixDims == 2
+if matrixDims == 1
+
+    z = fisher(r)
+
+elseif matrixDims == 2
     
     [a b] = size(r);
-    z = zeros(a, b);
+    z = nan(a, b);
     for ai = 1:a
         for bi = 1:b
-            num = 1 + r(ai, bi);
-            den = 1 - r(ai, bi);
-            z(ai, bi) = 0.5 * log(num / den);
+            z(ai, bi) = fisher(r(ai, bi));
         end
     end
     
 elseif matrixDims == 3
     
     [a b c] = size(r);
-    z = zeros(a, b, c);
+    z = nan(a, b, c);
     for ai = 1:a
         for bi = 1:b
             for ci = 1:c
-                num = 1 + r(ai, bi, ci);
-                den = 1 - r(ai, bi, ci);
-                z(ai, bi, ci) = 0.5 * log(num / den);
+                z(ai, bi, ci) = fisher(r(ai, bi, ci));
             end
         end
     end
@@ -39,4 +39,12 @@ else
     
     error('Cannot process %.f-dimensional matrices.', matrixDims)
     
+end
+
+end
+
+function z = fisher(r)
+
+z = 0.5 * log((1 + r) / (1 - r))
+
 end
